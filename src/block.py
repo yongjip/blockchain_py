bd7eff1d2de785bd293d1b17b3c7ac857fe3ec6f
dan <dan@danui-MacBook-Air.local>
2018-07-13 21:51:55 +0900

    add a couple of comments and tests

--- src/block.py
+++ src/block.py
@@ -16,12 +16,8 @@ class Block:
     """Docstring for Block. """
 
     def __init__(self, index, transactions, timestamp, previous_hash):
-        """TODO: to be defined1.
-
-        :index: TODO
-        :transactions: TODO
-        :timestamp: TODO
-
+        """
+        TODO: to be defined1.
         """
         self._index = []
         self._transactions = transactions
@@ -31,18 +27,15 @@ class Block:
     def compute_hash(self):
         """TODO: Docstring for compute_hash.
         :returns: TODO
-
+        해시를 생성합니다.
         """
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
 
 
 class BlockChain(object):
-
     """Docstring for BlockChain. """
-
     # difficulty of Pow algorithm
-
     difficulty = 2
 
     def __init__(self):
@@ -57,7 +50,9 @@ class BlockChain(object):
         A function to generate genesis block and appends it to
         the chain. the block has index 0, previous_hash as 0, and
         a valid hash
-        :returns: TODO
+        초기 블록을 생성합니다.
+        해당 블록은 인덱스가 0 이고, 이전해시값이 0입니다.
+        그리고 유효합니다.
 
         """
         genesis_block = Block(0, [], time.time(), "0")
@@ -68,3 +63,42 @@ class BlockChain(object):
     def last_block(self):
         """docstring for last_block"""
         return self.chain[-1]
+
+    def proof_of_work(self, block):
+        """TODO: Docstring for proof_of_work.
+        Function that tries different values of nonce to get a hash
+        that satisfies our difficulty criteria.
+
+        """
+        block.nonce = 0
+
+        computed_hash = block.compute_hash()
+        while not computed_hash.startwith('0' * BlockChain.difficulty):
+            block.nounce += 1
+            computed_hash = block.compute_hash()
+
+        return computed_hash
+
+    def add_block(self, block, proof):
+        """
+        A function that adds the block to the chain after verification
+        """
+
+        previous_hash = self.last_block.hash
+
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not self.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (block_hash.startwith('0' * BlockChain.difficulty) and block_hash == block.compute_hash())
--- src/tests/test_block.py
+++ src/tests/test_block.py
@@ -7,12 +7,33 @@ Description:
 """
 
 from src.tests.base import TestBaseClass
+from src.block import Block, BlockChain
 
 
 class TestBlockClass(TestBaseClass):
 
-    """Test case docstring."""
-
-
     def test_smoke_test(self):
+        """
+        Just simple smoke test for importing
+        단순 초기 스모크 테스트입니다.
+        """
         assert 1 is 1, "Just Small Import Test"
+
+    def test_create_genesis_block_chain(self):
+        """
+        test create genesis block
+        제네시스 블록(초깃값 블록) 을 생성하는 테스트입니다.
+        """
+        blk = BlockChain()
+        blk.create_genesis_block()
+
+        assert len(blk.chain) == 2
+
+    def test_create_genesis_block_and_get_last_block(self):
+
+        blk = BlockChain()
+        blk.create_genesis_block()
+
+        assert len(blk.chain) == 2
+
+        assert blk.last_block == blk.chain[-1]
